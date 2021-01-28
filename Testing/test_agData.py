d14a04f78ae56e7d6a6007e6f6aee364c8122c91
gonsongo <gonsongo@macalester.edu>
2021-01-28 16:46:23 -0600

    cleaned some files

--- Testing/test_agData.py
+++
@@ -1,5 +0,0 @@
-TRAIN_DATA = [
-    ('Eight-Twelve is a six-rowed winter feed barley.', {'entities': [(0, 12, 'CVAR'), (18, 27, 'TRAT'), (28, 34, 'TRAT'), (35, 39, 'CVAR'), (40, 46, 'CROP')]}),
-    ('It was released by the USDA-ARS and the Idaho AES in 1991.', {'entities': [(23, 31, 'ORG'), (40, 49, 'ORG'), (53, 57, 'DATE')]}),
-    ('It was selected from the cross Steveland/Luther//Wintermalt.', {'entities': [(31, 59, 'PED')]})
-]
