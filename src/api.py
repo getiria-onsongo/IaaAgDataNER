113bb33317a4b4d0a84b4aa3c9bc338fd55c77b2
gonsongo <gonsongo@macalester.edu>
2021-08-09 16:18:31 -0500

    Start tracking AgDataAPI.ipynb

--- src/api.py
+++ src/api.py
@@ -3,6 +3,41 @@ import psycopg2
 import pandas as pd
 from psycopg2 import Error
 
+def get_crop_varieties(crop="barley"):
+    crop = crop.lower()
+
+    try:
+        # Connect to an existing database
+        connection = psycopg2.connect(user="gonsongo",
+                                      password="",
+                                      host="localhost",
+                                      port="5432",
+                                      database="ner")
+
+        # Create a cursor to perform database operations
+        cursor = connection.cursor()
+
+        # Executing a SQL query
+        sql_query = "SELECT DISTINCT cvar FROM crop_data WHERE crop_name = '"+crop+"';"
+        cursor.execute(sql_query)
+
+        # Fetch result
+        records = cursor.fetchall()
+
+        # Create a pandas data frame with the results
+        rows = []
+        for record in records:
+            rows.append(record[0])
+        d = {'Crop Variety': rows}
+        df = pd.DataFrame(data=d)
+        return df
+    except (Exception, Error) as error:
+        print("Error while connecting to PostgreSQL", error)
+    finally:
+        if (connection):
+            cursor.close()
+            connection.close()
+
 def get_traits(crop="barley", variety='strider'):
     crop = crop.lower()
     variety = variety.lower()
@@ -32,15 +67,14 @@ def get_traits(crop="barley", variety='strider'):
         df = pd.DataFrame(data=d)
         return df
 
-
     except (Exception, Error) as error:
         print("Error while connecting to PostgreSQL", error)
     finally:
         if (connection):
             cursor.close()
             connection.close()
-            print("PostgreSQL connection is closed")
+            # print("PostgreSQL connection is closed")
 
 if __name__ == "__main__":
-    x = get_traits("Barley", "Maja")
-    print(x.head(1))
+    x = get_traits(crop="Barley", variety="Maja")
+    print(x.head(10))
