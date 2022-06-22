23e70a34c748eb224f0bc9ffe039ae7ce127cff0
Cora <38634654+afortiori@users.noreply.github.com>
2022-06-22 12:57:46 -0400

    medacy validation

--- src/medacyValidate.py
+++ src/medacyValidate.py
@@ -1,20 +1,11 @@
 from medacy.tools.calculators.inter_dataset_agreement import measure_dataset
 from medacy.tools.calculators.inter_dataset_agreement import format_results
 from medacy.data.dataset import Dataset
-import json
 
-gold_dataset = Dataset("Data/converted/DavisLJ11")
-spacy_dataset = Dataset("Data/predictions/DavisLJ11SpacyAnn")
-pos_dataset = Dataset("Data/predictions/DavisLJ11PosAnn")
 
-print("Spacy results")
-print("____________________________")
-result = measure_dataset(gold_dataset, spacy_dataset, 'lenient')
+gold_dataset = Dataset("Data/Input")
+system_dataset = Dataset("Data/Output")
+
+result = measure_dataset(gold_dataset, system_dataset)
 output = format_results(result)
-print(output)
-
-print("\nPOS results")
-print("____________________________")
-result = measure_dataset(gold_dataset, pos_dataset, 'lenient')
-output = format_results(result)
-print(output)
+print(result)
