8c3f77c77dc60f5f8667eebf57de1a6bf2248e5b
LethargicPuppy14 <ruben.escobar.c14@gmail.com>
2022-06-23 15:59:32 -0500

    Added more robust page loading (can now load ranges, and can't load pages that don't exist)

--- src/CropNerGUI.py
+++ src/CropNerGUI.py
@@ -15,8 +15,6 @@ import tkinter as tk
 from tkinter import filedialog as fd
 from tkinter.scrolledtext import ScrolledText
 
-import hashlib #TODO: Remove this, it's for debugging and was simply forgotten if still here
-
 # 1) WE NEED TO RESOLVE STANDARDIZING THINGS SUCH AS
 # ROUGH AWNS OR AWNS ARE ROUGH. NOTE: Maybe compound traits
 # do not make sense because we need to be able to know
@@ -500,6 +498,55 @@ class CropNerGUI:
         self.pdf_name = self.raw_file.name.split("/")[-1]
         self.pdf_document = Document(self.raw_file.name)
 
+    def page_num_is_valid(self, page_num):
+        """
+        Returns True if page_num is a number, False if page_num is completely invalid, and -1 if it's a range.
+        """
+        if page_num.isdigit():
+            return True
+        dash_index = page_num.find("-")
+        if(dash_index == -1):
+            return False
+        pg1 = page_num[0 : dash_index]
+        if not pg1.isdigit():
+            return False
+        pg2 = page_num[dash_index + 1:]
+        if not pg2.isdigit():
+            return False
+        return -1
+
+    def handle_bad_page_requests(self, page_num_valid):
+        """
+        If a page not in the doc is requested, this will set the page number to a valid
+        one and print a warning.
+
+        page_num_valid is passed in so that this method already knows whether it should
+        be checking a list or single number.
+        """
+        doc_length = len(self.pdf_document)
+        if page_num_valid == -1: # ie, page number is a range
+            if self.page_number[0] < 1: # This will only take effect if the starting page is set to 0. Beginning hyphens are already invalid.
+                self.page_number[0] = 1
+                self.msg.config(text="First page entered is less than 1; setting first page to 1", foreground="red")
+                self.page_entry.delete(0, tk.END)
+                self.page_entry.insert(0, "1-" + str(self.page_number[1]))
+            if self.page_number[1] > doc_length:
+                self.page_number[1] = doc_length
+                self.msg.config(text="Last page entered is greater than the length of the PDF; setting last page to the end of the PDF")
+                self.page_entry.delete(0, tk.END)
+                self.page_entry.insert(0, str(self.page_number[0]) + "-" + str(self.page_number[1]))
+        else: # ie. there is a single page
+            if self.page_number < 1: # This may as well be self.page_number == 0. A beginning hyphen is already invalid (not a digit or a range).
+                self.page_number = 1
+                self.msg.config(text="Page entered is too small; setting to 1", foreground="red")
+                self.page_entry.delete(0, tk.END)
+                self.page_entry.insert(0, "1")
+            if self.page_number > doc_length:
+                self.page_number = doc_length
+                self.msg.config(text="Page entered is too large; setting it to the last page (" + str(doc_length) + ")", foreground="red")
+                self.page_entry.delete(0, tk.END)
+                self.page_entry.insert(0, str(doc_length))
+
     def load_page(self):
         """
         Load contents of a PDF or text file into text box.
@@ -507,7 +554,6 @@ class CropNerGUI:
         If the entry box for page number has a value, it will load the page specified. If not, by default it will
         load the first page.
         """
-        # TODO: Currently only loads 1 page. Update to load arbitrary number of pages (max=size of document).
         # TODO: Give users the option to load text files in addition to pdf files.
         if self.raw_file is None:
             self.msg.config(text="No raw data file has been selected. Please select a file to load.", foreground="red")
@@ -516,29 +562,45 @@ class CropNerGUI:
             # Reset annotation dictionary
             self.cust_ents_dict = {}
 
-            page_num = self.page_entry.get()
-            if not page_num.isdigit():
-                self.msg.config(text="Page number not entered. Value initialized to 1", foreground="red")
+            page_num = self.page_entry.get().replace(" ", "")
+            page_num_valid = self.page_num_is_valid(page_num)
+            if page_num_valid == False:
+                self.msg.config(text="Valid page number not entered. Value initialized to 1", foreground="red")
                 self.page_number = 1
                 self.page_entry.delete(0,tk.END)
                 self.page_entry.insert(0, str(self.page_number))
-            else:
+                self.chunk=self.page_number
+            elif page_num_valid == True:
                 self.page_number = int(page_num)
+                self.chunk=self.page_number
+            else: # Range of numbers
+                dash_index = page_num.find("-")
+                pg1 = int(page_num[0 : dash_index])
+                pg2 = int(page_num[dash_index + 1:])
+                self.page_number = [pg1, pg2]
+                # Switch first and last page if the user's first number was the smaller one.
+                if(self.page_number[1] < self.page_number[0]):
+                    self.msg.config(text="A larger page was entered first. The pages will be displayed in order from smallest to largest anyway.", foreground="red")
+                    placeholder = self.page_number[0]
+                    self.page_number[0] = self.page_number[1]
+                    self.page_number[1] = placeholder
+                self.chunk=self.page_number[0]
 
-            self.chunk=self.page_number
             # Delete contents
             self.text.delete(1.0, tk.END)
 
             # Load PDF file
-            # if self.pdf_document is None:
             self.load_pdf()
 
-            # Extract text from pdf while maintaining layout
-            # control = TextControl(mode="physical")
-            # We don't want to maintain layout, as this breaks any PDF with multiple columns.
+            self.handle_bad_page_requests(page_num_valid)
 
-            page = self.pdf_document[self.page_number - 1]
-            txt = page.text().replace("\r", "")
+            if not (page_num_valid == -1): # Single page, whether page_num_valid is true or false
+                page = self.pdf_document[self.page_number - 1]
+                txt = page.text().replace("\r", "")
+            else: # Page range
+                txt = ""
+                for page in self.pdf_document[self.page_number[0] - 1 : self.page_number[1]]:
+                    txt = txt + page.text().replace("\r", "")
             self.text.insert("1.0",txt)
 
     def update_scrolled_text_line_content_index(self):
