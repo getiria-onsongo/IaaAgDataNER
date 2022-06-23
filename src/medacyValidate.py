ded78964e9db977a09575082bec39f0b1bba1ddf
Cora <38634654+afortiori@users.noreply.github.com>
2022-06-23 09:51:23 -0400

    medacy validation code updates

--- src/medacyValidate.py
+++ src/medacyValidate.py
@@ -1,11 +1,27 @@
 from medacy.tools.calculators.inter_dataset_agreement import measure_dataset
 from medacy.tools.calculators.inter_dataset_agreement import format_results
 from medacy.data.dataset import Dataset
+import argparse
+import json
+import pprint
+from medacy.data.annotations import Annotations
+from medacy.data.data_file import DataFile
 
 
-gold_dataset = Dataset("Data/Input")
-system_dataset = Dataset("Data/Output")
+gold_dataset = Dataset("Data/InputDavis")
+system_dataset = Dataset("Data/OutputDavis")
 
-result = measure_dataset(gold_dataset, system_dataset)
+
+# for file in system_dataset:
+#     print(file.file_name, str(file.txt_path), str(file.ann_path))
+
+# entities = json.dumps(gold_dataset.get_labels(as_list=True))
+# counts = gold_dataset.compute_counts()
+# print(f"Entities: {entities}")
+# pprint.pprint(counts)
+
+
+
+result = measure_dataset(gold_dataset, system_dataset, 'lenient')
 output = format_results(result)
 print(result)
