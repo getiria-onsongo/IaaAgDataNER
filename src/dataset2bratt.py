804430ef3e28a8aafbb101dfa97a3f4d460632b3
Cora <38634654+afortiori@users.noreply.github.com>
2022-06-24 11:32:19 -0400

    dataset to bratt minor updates

--- src/dataset2bratt.py
+++ src/dataset2bratt.py
@@ -1,8 +1,9 @@
 from json2bratt import conversion
 import glob
+import argparse
 
 
-def dataset_to_bratt(file_dir, output_dir, file_pattern="/*_td.json", name_prefix=None):
+def dataset_to_bratt(input_dir, output_dir, file_pattern="/*td.json", name_prefix=None):
     '''
     Converts a whole dataset into bratt and txt files
 
@@ -17,13 +18,43 @@ def dataset_to_bratt(file_dir, output_dir, file_pattern="/*_td.json", name_prefi
     name_prefix : str
         (optional) start of name for new bratt and json files
     '''
-    files = glob.glob(file_dir+file_pattern)
+    files = glob.glob(input_dir+file_pattern)
     print("%s files to convert." % str(len(files)))
 
     for f in files:
         print("Converting %s..." % f)
-        if name_prefix == None:
-            name_prefix = f.split(".")[0].split("/")[2]
-        conversion(f, output_dir+"/"+name_prefix)
+        if name_prefix is None:
+            prefix = f.split(".")[0].split("/")[2]
+        else:
+            prefix = name_prefix
+        conversion(f, output_dir+"/"+prefix)
 
     print("Finished dataset conversion, new files in %s." % output_dir)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="Converts a dataset to bratt",
+        epilog='python dataset2bratt.py input_dir output_dir'
+    )
+    parser.add_argument(
+        'input_dir', help='path to files to convert'
+    )
+    parser.add_argument(
+        'output_dir', help='path to output converted files'
+    )
+
+    parser.add_argument(
+        '--file_pattern', help='file ending of files to convert',
+        action='store', default="/*_td.json"
+    )
+    parser.add_argument(
+            '--name_prefix', help='file name prefix for saving',
+            action='store', default=None
+    )
+
+    args = parser.parse_args()
+    input, output = args.input_dir, args.output_dir
+    pattern, prefix = args.file_pattern, args.name_prefix
+
+    dataset_to_bratt(input, output, file_pattern=pattern, name_prefix=prefix)
