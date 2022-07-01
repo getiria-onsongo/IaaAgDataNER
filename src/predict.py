dbbe7a527c9fca08e6afee28b56195c8a62c35c9
Cora <38634654+afortiori@users.noreply.github.com>
2022-07-01 14:22:23 -0400

    bit better name extraction

--- src/predict.py
+++ src/predict.py
@@ -251,7 +251,7 @@ class Predict:
         name = self.json_prefix
         if name == None:
             path_no_suffix = pdf_name.split(self.dataset_suffix)[0].split("/")
-            name = path_no_suffix[len(path_no_suffix)-1].split("p")[0] + "p"
+            name = path_no_suffix[len(path_no_suffix)-1].split("_p")[0] + "_p"
             print(name)
         output_filename = self.output_dir + "/" + name + chunk + self.json_suffix
 
