0b75dec4a2e4057d840c98919da492483d1fdfe7
Cora <38634654+afortiori@users.noreply.github.com>
2022-07-05 15:27:18 -0400

    now using internal spacy api and added evaluation for k fold cross val

--- src/crossvalidation.py
+++ src/crossvalidation.py
@@ -1,26 +1,38 @@
-from sklearn.model_selection import KFold
 from json2SpacyJson import convertJsonToSpacyJsonl
 from validation_testing import execute
+from spacy.cli.train import train
+from spacy.cli.convert import convert
 import glob
 import os
 from agParse import *
 import random
 
+
 def cross_validate(fold_number, data, pos_split):
-    # divide data into folds
+    # shuffles and divides data into k folds and a dev set
     files = glob.glob(data+"/*.json") # add feature to search multiple dirs?
     random.shuffle(files)
-    files_per_fold = (len(files) // fold_number) + 1 # should I round up or down?
+    files_per_divison = (len(files) // fold_number) + 2 # should I round up or down?
     folds = []
     start,end = 0,1
     for i in range(0, fold_number):
         if i != files_per_fold:
-            folds.append(files[files_per_fold*start:files_per_fold*end])
+            folds.append(files[files_per_divison*start:files_per_divison*end])
             start += 1
             end += 1
         else:
-            folds.append(files[files_per_fold*start:len(files)])
+            folds.append(files[files_per_divison*start:len(files)])
 
+    # makes spacy binary output dir if it doesn't exist
+    if not os.path.exists("ner_2021_08"):
+        os.makedirs("ner_2021_08")
+
+    # create and convert dev set
+    dev = folds[fold_number]
+    convertJsonToSpacyJsonl(outputFileName="ner_2021_08_dev_data.jsonl", filePaths=dev)
+    convert(input_path="ner_2021_08_dev_data.jsonl", output_dir="ner_2021_08")
+
+    # k-fold cross validation
     for v in range(0, fold_number):
         # train - validate split
         training = []
@@ -34,18 +46,18 @@ def cross_validate(fold_number, data, pos_split):
         convertJsonToSpacyJsonl(outputFileName="ner_2021_08_validate_data.jsonl", filePaths=validation)
 
         # spacy json to spacy binary
-        if not os.path.exists("ner_2021_08"):
-            os.makedirs("ner_2021_08")
-        execute("python3 -m spacy convert --converter json ner_2021_08_training_data.jsonl ner_2021_08")
-        execute("python3 -m spacy convert --converter json ner_2021_08_validate_data.jsonl ner_2021_08")
+        convert(input_path="ner_2021_08_training_data.jsonl", output_dir="ner_2021_08")
+        convert(input_path="ner_2021_08_validate_data.jsonl", output_dir="ner_2021_08")
 
         # train model
-        execute("python3 -m spacy train senter_ner.cfg --output ner_2021_08_model --paths.train ner_2021_08/ner_2021_08_training_data.spacy --paths.dev ner_2021_08/ner_2021_08_validate_data.spacy")
+        train(config_path="../senter_ner.cfg", output_path="ner_2021_08_model", overrides={"paths.train": "ner_2021_08/ner_2021_08_training_data.spacy", "paths.dev": "ner_2021_08/ner_2021_08_validate_data.spacy"})
+
+        # evaulate model
+        out_metrics_path = "metrics_fold_" + str(v) + "_.json"
+        evaluate(model="ner_2021_08_model", data_path="ner_2021_08/ner_2021_08_dev_data.spacy", output_path=out_metrics_path)
 
         if pos_split:
-            pass
-            # pos post-processing here
-
+            print("Not implemented.")
 
 
 if __name__ == '__main__':
@@ -67,4 +79,4 @@ if __name__ == '__main__':
             help='do pos tagging after model training'
     )
     args = parser.parse_args()
-    cross_validate(args.folds, args.dataset_dir, args.pos_tagging)
+    cross_validate(int(args.folds), args.dataset_dir, args.pos_tagging)
