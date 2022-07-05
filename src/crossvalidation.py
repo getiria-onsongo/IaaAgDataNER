615c511e99912163a6b8ba725ec917d1c283976b
Cora <38634654+afortiori@users.noreply.github.com>
2022-07-05 10:43:33 -0400

    finished draft of cross validate

---
+++ src/crossvalidation.py
@@ -0,0 +1,53 @@
+from sklearn.model_selection import KFold
+from json2SpacyJson import convertJsonToSpacyJsonl
+from validation_testing import execute
+import glob
+import os
+import random
+
+def cross_validate(fold_number, data, pos_split):
+    # divide data into folds
+    files = glob.glob(data+"/*.json") # add feature to search multiple dirs?
+    random.shuffle(files)
+    files_per_fold = (len(files) // fold_number) + 1 # should I round up or down?
+    folds = []
+    start,end = 0,1
+    for i in range(0, fold_number):
+        if i != files_per_fold:
+            folds.append(files[files_per_fold*start:files_per_fold*end])
+            start += 1
+            end += 1
+        else:
+            folds.append(files[files_per_fold*start:len(files)])
+
+    for v in range(0, fold_number):
+        # train - validate split
+        training = []
+        for t in range(0, fold_number):
+            if t != v:
+                training += folds[t]
+        validation = folds[v]
+
+        # json to spacy json
+        convertJsonToSpacyJsonl(outputFileName="ner_2021_08_training_data.jsonl", filePaths=training)
+        convertJsonToSpacyJsonl(outputFileName="ner_2021_08_validate_data.jsonl", filePaths=validation)
+
+        # spacy json to spacy binary
+        if not os.path.exists("ner_2021_08"):
+            os.makedirs("ner_2021_08")
+        execute("python3 -m spacy convert --converter json ner_2021_08_training_data.jsonl ner_2021_08")
+        execute("python3 -m spacy convert --converter json ner_2021_08_validate_data.jsonl ner_2021_08")
+
+        # train model
+        execute("python3 -m spacy train senter_ner.cfg --output ner_2021_08_model --paths.train ner_2021_08/ner_2021_08_training_data.spacy --paths.dev ner_2021_08/ner_2021_08_validate_data.spacy")
+
+        if pos_split:
+            pass
+            # pos post-processing here
+
+
+
+
+
+
+cross_validate(5, "Data/DavisLJ11", False)
