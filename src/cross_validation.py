ee92aa54a97970dc072697ddd710e5593131b2d0
Cora <38634654+afortiori@users.noreply.github.com>
2022-07-06 16:58:23 -0400

    added option for pos tagging to cross validate

--- src/cross_validation.py
+++ src/cross_validation.py
@@ -7,16 +7,32 @@ from validation_testing import execute
 from agParse import *
 import glob
 import os
+import json
 import random
 from predict import Predict
+from dataset2bratt import dataset_to_bratt
 
 class CrossValidation:
+    """
+    A class to do k-fold cross validation for a Spacy model
+
+    ...
+    Attributes
+    ----------
+    self.k_folds : int
+        number of folds
+    self.tags : list[str]
+        labels for ner entities
+
+    Methods
+    ----------
+    """
     def __init__(self, k_folds=5, tags=["ALAS", "CROP", "CVAR", "PATH", "PED", "PLAN", "PPTD", "TRAT"]):
         self.k_folds = k_folds
         self.tags = tags
 
     def cross_validate(self, data, pos_split):
-    # shuffles and divides data into k folds and a dev set
+        # shuffles and divides data into k folds and a dev set
         splits = self.k_folds + 1
         files = glob.glob(data+"/*.json") # add feature to search multiple dirs?
         random.shuffle(files)
@@ -31,9 +47,9 @@ class CrossValidation:
             else:
                 folds.append(files[files_per_divison*start:len(files)])
         print(folds)
+
         # makes spacy binary output dir if it doesn't exist
-        if not os.path.exists("ner_2021_08"):
-            os.makedirs("ner_2021_08")
+        self.create_dirs(["ner_2021_08"])
 
         # create and convert the dev set
         # this is used for every fold's spacy training & doesn't change
@@ -50,23 +66,37 @@ class CrossValidation:
                     training += folds[t]
             validation = folds[v]
 
-            # convert training data
+            # convert training data into spacy json and then into spacy binary
             convertJsonToSpacyJsonl(outputFileName="ner_2021_08_training_data.jsonl", filePaths=training)
             convert(input_path="ner_2021_08_training_data.jsonl", output_dir="ner_2021_08", converter="json", file_type="spacy")
-
 
             # train model
             train(config_path="senter_ner.cfg", output_path="cv_2021_08_model", overrides={"paths.train": "ner_2021_08/ner_2021_08_training_data.spacy", "paths.dev": "ner_2021_08/ner_2021_08_dev_data.spacy"})
 
-
             # evaulate model
             if pos_split:
+                # create needed directories
+                json_name = "pos_cv_val_" + str(v) + "_json"
+                bratt_name = "pos_cv_val_" + str(v) + "_bratt"
+                gold_json_name = "pos_cv_val_" + str(v) + "_gold_json"
+                gold_bratt_name = "pos_cv_val_" + str(v) + "_gold_bratt"
+                self.create_dirs([json_name, bratt_name, gold_json_name, gold_bratt_name])
+
                 # do pos tagging & entity expansion
-                name = "pos_cv_output" + str(v)
-                predict = Predict("cv_2021_08_model/model-best", name)
-                predict.preprocess(validate)
-                # evaluate
-                # need to implement
+                predict = Predict("cv_2021_08_model/model-best", json_name)
+                predict.preprocess(validation)
+
+                # create gold standard dataset and convert both to bratt
+                # can now be run through medacy's inter_dataset_agreement tool for evaulation
+                for file in validation:
+                    json_file = open(file)
+                    contents = json.load(json_file)
+                    ls = file.split("/")
+                    file_name = ls[len(ls)-1]
+                    with open(gold_json_name+"/"+file_name, 'w') as f:
+                        json.dump(contents, f)
+                dataset_to_bratt(gold_json_name, gold_bratt_name)
+                dataset_to_bratt(json_name, bratt_name)
 
             else:
                 # convert validate data
@@ -76,7 +106,6 @@ class CrossValidation:
 
                 # evaluate
                 evaluate(model="ner_2021_08_model/model-best", data_path="ner_2021_08/ner_2021_08_validate_data.spacy", output=output_name)
-
 
     def extract_metrics(self, prefix="metrics_fold", suffix=".json"):
         metrics = defaultdict(list)
@@ -120,6 +149,11 @@ class CrossValidation:
                 print("\t precesion: " + str(metrics[k][0]))
                 print("\t recall: " + str(metrics[k][1]))
                 print("\t F1: " + str(metrics[k][2]))
+
+    def create_dirs(self, dirs):
+        for dir in dirs:
+            if not os.path.exists(dir):
+                os.makedirs(dir)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
