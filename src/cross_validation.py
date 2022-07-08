f1ffac33f1c24b0dcb1a9bf66c0dc136289fc591
Cora <38634654+afortiori@users.noreply.github.com>
2022-07-08 11:45:29 -0400

    changed to strict medacy eval

--- src/cross_validation.py
+++ src/cross_validation.py
@@ -157,33 +157,33 @@ class CrossValidation:
     def medacy_eval(self):
         print("Fold 0")
         print("____________________________")
-        result = measure_dataset(Dataset("fold_0_results/gold_bratt"), Dataset("fold_0_results/pred_bratt"), 'lenient')
+        result = measure_dataset(Dataset("fold_0_results/gold_bratt"), Dataset("fold_0_results/pred_bratt"), 'strict')
         output = format_results(result)
         print(output)
 
         print("\nFold 1")
         print("____________________________")
-        result = measure_dataset(Dataset("fold_1_results/gold_bratt"), Dataset("fold_1_results/pred_bratt"), 'lenient')
+        result = measure_dataset(Dataset("fold_1_results/gold_bratt"), Dataset("fold_1_results/pred_bratt"), 'strict')
         output = format_results(result)
         print(output)
 
 
         print("\nFold 2")
         print("____________________________")
-        result = measure_dataset(Dataset("fold_2_results/gold_bratt"), Dataset("fold_2_results/pred_bratt"), 'lenient')
+        result = measure_dataset(Dataset("fold_2_results/gold_bratt"), Dataset("fold_2_results/pred_bratt"), 'strict')
         output = format_results(result)
         print(output)
 
 
         print("\nFold 3")
         print("____________________________")
-        result = measure_dataset(Dataset("fold_3_results/gold_bratt"), Dataset("fold_3_results/pred_bratt"), 'lenient')
+        result = measure_dataset(Dataset("fold_3_results/gold_bratt"), Dataset("fold_3_results/pred_bratt"), 'strict')
         output = format_results(result)
         print(output)
 
         print("\nFold 4")
         print("____________________________")
-        result = measure_dataset(Dataset("fold_4_results/gold_bratt"), Dataset("fold_4_results/pred_bratt"), 'lenient')
+        result = measure_dataset(Dataset("fold_4_results/gold_bratt"), Dataset("fold_4_results/pred_bratt"), 'strict')
         output = format_results(result)
         print(output)
 
