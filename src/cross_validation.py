fe5410a59f5cebf705f18782738aa0cf11aade92
Cora <38634654+afortiori@users.noreply.github.com>
2022-07-08 13:07:52 -0400

    leninet medacy eval

--- src/cross_validation.py
+++ src/cross_validation.py
@@ -157,33 +157,33 @@ class CrossValidation:
     def medacy_eval(self):
         print("Fold 0")
         print("____________________________")
-        result = measure_dataset(Dataset("fold_0_results/gold_bratt"), Dataset("fold_0_results/pred_bratt"), 'strict')
+        result = measure_dataset(Dataset("fold_0_results/gold_bratt"), Dataset("fold_0_results/pred_bratt"), 'lenient')
         output = format_results(result)
         print(output)
 
         print("\nFold 1")
         print("____________________________")
-        result = measure_dataset(Dataset("fold_1_results/gold_bratt"), Dataset("fold_1_results/pred_bratt"), 'strict')
+        result = measure_dataset(Dataset("fold_1_results/gold_bratt"), Dataset("fold_1_results/pred_bratt"), 'lenient')
         output = format_results(result)
         print(output)
 
 
         print("\nFold 2")
         print("____________________________")
-        result = measure_dataset(Dataset("fold_2_results/gold_bratt"), Dataset("fold_2_results/pred_bratt"), 'strict')
+        result = measure_dataset(Dataset("fold_2_results/gold_bratt"), Dataset("fold_2_results/pred_bratt"), 'lenient')
         output = format_results(result)
         print(output)
 
 
         print("\nFold 3")
         print("____________________________")
-        result = measure_dataset(Dataset("fold_3_results/gold_bratt"), Dataset("fold_3_results/pred_bratt"), 'strict')
+        result = measure_dataset(Dataset("fold_3_results/gold_bratt"), Dataset("fold_3_results/pred_bratt"), 'lenient')
         output = format_results(result)
         print(output)
 
         print("\nFold 4")
         print("____________________________")
-        result = measure_dataset(Dataset("fold_4_results/gold_bratt"), Dataset("fold_4_results/pred_bratt"), 'strict')
+        result = measure_dataset(Dataset("fold_4_results/gold_bratt"), Dataset("fold_4_results/pred_bratt"), 'lenient')
         output = format_results(result)
         print(output)
 
@@ -298,7 +298,8 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     val = CrossValidation(k_folds=int(args.folds))
-    val.cross_validate(args.dataset_dir, args.pos_tagging)
-    if args.pos_tagging is False:
-        avgs = val.average_metrics(val.extract_metrics())
-        val.format_metrics(avgs)
+    val.medacy_eval()
+    # val.cross_validate(args.dataset_dir, args.pos_tagging)
+    # if args.pos_tagging is False:
+    #     avgs = val.average_metrics(val.extract_metrics())
+    #     val.format_metrics(avgs)
