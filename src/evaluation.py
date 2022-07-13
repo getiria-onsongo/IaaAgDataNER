25c994343c1cb8f298e42a0dd4309391215c44f7
Cora <38634654+afortiori@users.noreply.github.com>
2022-07-13 13:50:06 -0400

    fixed eval script

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,13 +1,5 @@
 from cross_validation import CrossValidation
 
-class evaluation:
-    def __init__(model):
-        self.model = model
-    def evaluate():
-        val = crossvalidation()
-        avg_metrics, found_ents = val.medacy_eval()
-        val.print_metrics(avg_metrics, found_ents)
-
-if __name__ == '__main__':
-    eval = evaluation()
-    eval.evaluate()
+val = CrossValidation()
+avg_metrics, found_ents = val.medacy_eval()
+val.print_metrics(avg_metrics, found_ents)
