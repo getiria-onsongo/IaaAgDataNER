761908ff3cdcad7140764b51fd51f6dff0071d9f
Cora <38634654+afortiori@users.noreply.github.com>
2022-07-20 16:42:14 -0400

    no more background tasks

--- batch_run.sh
+++ batch_run.sh
@@ -1,27 +1,23 @@
 # page level (standard)
 mkdir jul20_pg_level
 cd jul20_pg_level
-nohup python ~/packages/IaaAgDataNER/src/cross_validation.py ~/packages/IaaAgDataNER/Data/DavisLJ11 --pos &
-wait $!
+nohup python ~/packages/IaaAgDataNER/src/cross_validation.py ~/packages/IaaAgDataNER/Data/DavisLJ11 --pos
 cd ..
 
 # glove word embeddings
 mkdir jul20_pg_level_word_embed
 cd jul20_pg_level_word_embed
-nohup python ~/packages/IaaAgDataNER/src/cross_validation.py ~/packages/IaaAgDataNER/Data/DavisLJ11 --word_embed --vectors ~/packages/IaaAgDataNER/glove.6B.zip &
-wait $!
+nohup python ~/packages/IaaAgDataNER/src/cross_validation.py ~/packages/IaaAgDataNER/Data/DavisLJ11 --word_embed --vectors ~/packages/IaaAgDataNER/glove.6B.zip
 cd ..
 
 # GPU
 mkdir jul20_pg_level_gpu
 cd jul20_pg_level_gpu
-nohup python ~/packages/IaaAgDataNER/src/cross_validation.py ~/packages/IaaAgDataNER/Data/DavisLJ11 --GPU &
-wait $!
+nohup python ~/packages/IaaAgDataNER/src/cross_validation.py ~/packages/IaaAgDataNER/Data/DavisLJ11 --GPU
 cd ..
 
 # GPU
 mkdir jul20_pg_level_spancat
 cd jul20_pg_level_spancat
-nohup python ~/packages/IaaAgDataNER/src/cross_validation.py ~/packages/IaaAgDataNER/Data/DavisLJ11 --spancat &
-wait $!
+nohup python ~/packages/IaaAgDataNER/src/cross_validation.py ~/packages/IaaAgDataNER/Data/DavisLJ11 --spancat
 cd ..
