c3d38697d8ab97b776dbabfef0f25adb1f1164f8
Cora <38634654+afortiori@users.noreply.github.com>
2022-08-03 15:39:12 -0400

    spancat ents to spans conversion

---
+++ src/add_ents_to_spans_dict.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+import typer
+import spacy
+from spacy.tokens import DocBin
+
+
+"""
+This function converts ner entities to spans to be able to use SpaCy's
+spancat for ner.
+From https://github.com/explosion/projects/blob/v3/experimental/ner_spancat/scripts/add_ents_to_spans_dict.py
+"""
+
+def convert_to_span(loc: Path, lang: str, span_key: str):
+    """
+    Set the NER data into the doc.spans, under a given key.
+
+    The SpanCategorizer component uses the doc.spans, so that it can work with
+    overlapping or nested annotations, which can't be represented on the
+    per-token level.
+    """
+    nlp = spacy.blank(lang)
+    docbin = DocBin().from_disk(loc)
+    docs = list(docbin.get_docs(nlp.vocab))
+    for doc in docs:
+        doc.spans[span_key] = list(doc.ents)
+    DocBin(docs=docs).to_disk(loc)
