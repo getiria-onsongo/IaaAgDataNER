d1a04e0ecefe5a8e80705f9cf418bf994da0f7b8
Cora <38634654+afortiori@users.noreply.github.com>
2022-08-03 15:38:37 -0400

    brat conversion code

---
+++ src/dataset2brat.py
@@ -0,0 +1,94 @@
+from json2brat import conversion
+import glob
+import argparse
+import os
+
+"""
+File to convert a json dataset to brat, also has page number extraction helper function.
+
+Functions
+-------
+extract_page_num(self, f : str, suffix : str) -> str
+    extracts page numbers from file names
+dataset_to_bratt(input_dir : str, output_dir : str, file_pattern : str, sentence_level : bool)
+    converts a json dataset to brat format
+"""
+
+def extract_page_num(f : str, suffix : str) -> str:
+    """
+    Gets page number for files where the page numbers are the one or two
+    positive integers found directly before the file suffix
+
+    Parameters
+    ----------
+    f : str
+        file name
+    suffix : str
+        file ending, everything after page numbers
+
+    Returns page number as string, otherwise returns empty string
+    """
+    num = ""
+    if f[len(f)-(len(suffix)+1)].isdigit():
+        num = f[len(f)-(len(suffix)+1)]
+        if f[len(f)-(len(suffix)+2)].isdigit():
+            num = f[len(f)-(len(suffix)+2)] + num
+    return num
+
+def dataset_to_brat(input_dir : str, output_dir : str, file_pattern="/*_td.json", sentence_level=False):
+    """
+    Converts a whole dataset into bratt and txt files.
+
+    Parameters
+    ----------
+    file_dir : str
+        directory of files to convert
+    output_dir : str
+        dirctory to output to
+    file_pattern : str
+        pattern of file endings in directory to select and convert,
+        default is "/*_td.json"
+    name_prefix : str
+        start of name for new bratt and json files
+    """
+    # retrive files
+    files = glob.glob(input_dir+"/**/*.json", recursive=True)
+    print("%s files to convert." % str(len(files)))
+
+    # convert
+    for f in files:
+        path_no_suffix = f.split(".json")[0].split("/")
+        prefix = path_no_suffix[len(path_no_suffix)-1]
+        conversion(f, output_dir+"/"+prefix, sentence_level)
+
+    print("Finished dataset conversion, new files in %s." % output_dir)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="Converts a dataset to brat",
+        epilog='python dataset2bratt.py input_dir output_dir'
+    )
+    parser.add_argument(
+        'input_dir', help='path to files to convert'
+    )
+    parser.add_argument(
+        'output_dir', help='path to output converted files'
+    )
+
+    parser.add_argument(
+        '--file_pattern', help='file ending of files to convert',
+        action='store', default="/*_td.json"
+    )
+    parser.add_argument(
+        '--sentence_level', help = 'flag to convert at sentence level',
+        action='store_true', default=False
+    )
+
+    args = parser.parse_args()
+
+    if not os.path.exists(args.input_dir):
+        print("input dataset path does not exist")
+    else:
+        if not os.path.exists(args.output_dir):
+            os.makedirs(args.output_dir)
+        dataset_to_brat(args.input_dir, args.output_dir, file_pattern=args.file_pattern, sentence_level= args.sentence_level)
---
+++ src/json2brat.py
@@ -0,0 +1,118 @@
+'''Documentation
+This is a json to bratt(ann) and text file converter
+It loads the json file and uses key in the nested dictionary
+Ann file uses Tindex \tabKeyterm start end \tabSentence
+The text file just combines all the sentences into one paragraph
+To use the program directly you would be asked for an input json file and asked to name both the ann and the txt file on one input
+If you are calling the function you would add the location of the json file and just the prefix
+'''
+
+import json
+import os.path
+import tkinter.filedialog
+
+# Checks if the python file is getting executed directly or getting called
+if __name__ == "__main__":
+    # Use tkinter for selecting files using a gui file selector
+    root = tkinter.Tk()
+    infile = tkinter.filedialog.askopenfilename(
+        initialdir=["../Data"], filetypes=[("json files", "*.json *.jsonl")])
+    root.destroy()
+    outfile = input("\nName your output file: ")
+
+
+def conversion(infile, outfile, sentence_level=False, print_statements=False):
+    annfile = outfile + ".ann"
+    txtfile = outfile + ".txt"
+
+    # If statement checks if the file exists and deletes it because the files will be appended not overwritten
+    if os.path.exists(annfile):
+        os.remove(annfile)
+        if print_statements:
+            print(f"\n{annfile} exists so it will be overwritten")
+    if os.path.exists(txtfile):
+        os.remove(txtfile)
+        if print_statements:
+            print(f"{annfile} exists so it will be overwritten")
+            print()
+    # Load the json file
+    with open(infile) as f:
+        data = json.load(f)
+
+    # First spilt the data by the sentences
+    data2 = data['sentences'].keys()
+
+    counter = 0
+    tindx = 1
+
+    # Key terms for all the labels in the json file
+    keyterm_dict = {
+        "ALAS": "varietal_alias",
+        "CROP": "crop",
+        "CVAR": "crop_variety",
+        "JRNL": "journal_reference",
+        "PATH": "pathogen",
+        "PED": "pedigree",
+        "PLAN": "plant_anatomy",
+        "PPTD": "plant_predisposition_to_disease",
+        "TRAT": "trait",
+        # not complete for given CSV!
+    }
+    # For each loop to list out the sentences
+    for x in data2:
+        i = 1
+        while i <= len(data['sentences'][x]):
+            try:
+                # Taking out the entities by using a while
+                entity = data['sentences'][x]['entity ' + str(i)]
+
+                # Split the entity datas and store into an array
+                entitydata = str(entity).split(" ")
+
+                # startnum and endnum add the length of the sentences the come before them
+
+                startnum = int(entitydata[1].replace(",", "")) + counter
+                endnum = int(entitydata[3].replace(",", "")) + counter
+                keyterm = entitydata[5].replace("'", "").replace("}", "")
+
+                # Replace the label using the keyterm dictionary above
+                keyterm_long = keyterm_dict.get(keyterm, None)
+                # If keyterm exists then print it out or print "unknown keyterm"
+                if keyterm_long:
+                    substring = x[int(entitydata[1].replace(",", "")): int(
+                        entitydata[3].replace(",", ""))]
+
+                    # File is outputted in this formatted seen below
+                    t_entry = f"T{tindx}\t{keyterm} {startnum} {endnum}\t{substring}"
+
+                    # Finally it's written here into the ann file
+                    with open(annfile, "a") as f:
+                        f.write(t_entry)
+                        f.write("\n")
+
+                else:
+                    if print_statements:
+                        print(f"\n{x} sentence has been skipped because, \033[91m\033[1m{keyterm}\033[0m is not defined in the dictionary")
+
+            # Exception if the sentence has not been formatted properly
+            except:
+                raise ValueError(f"\033[91m{x}\033[0m is not formatted correctly so it has been skipped")
+
+            tindx += 1
+            i += 1
+
+        # Text file is written here and joined without any space
+        with open(txtfile, "a") as f:
+            f.write(x)
+        # This is the part which decides if you wanna do document or sentence level
+        if sentence_level:
+            counter += len(x)
+        else:
+            counter = 0
+
+    print(f"File converted to bratt as {annfile} and {txtfile}")
+
+
+# Calls the method if the script is getting executed directly
+if __name__ == "__main__":
+    conversion(infile, outfile)
